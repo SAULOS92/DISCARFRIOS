4e9a224af82a5752ddc19c29ed491249ec3d996c
unknown <saulosorioh@hotmail.com>
2023-06-09 04:12:10 -0500

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { UsersService } from "../servicios/firebase/users.service";
+import { LoadingController } from '@ionic/angular';
 
 
 
@@ -18,7 +19,9 @@ export class HomePage {
   datos={};
   user_mob='';
   pass_mob='';
+  loading: any;
   constructor(public authService: UsersService, public router:Router, public toast:ToastController, 
+    private loadingController: LoadingController
     ) 
     {
       
@@ -26,12 +29,15 @@ export class HomePage {
     }
     ionViewWillEnter(){ }
       
-    logIn(email: any, password: any) {
+    async logIn(email: any, password: any) {
+      this.presentLoading();
       this.authService.SignIn(email, password)
         .then((res) => {
            this.verifyRegister();
+           this.dismissLoading()
         }).catch((error) => {
-          this.presentToast(error.message);          
+          this.presentToast(error.message);   
+          this.dismissLoading()       
         })
     }
     async presentToast(message1: any) {
@@ -43,6 +49,7 @@ export class HomePage {
     }
     verifyRegister()
     {  
+          this.presentLoading();
           //validar que el usuario este autenticado
           this.authService.ngFireAuth.authState.subscribe((user) => {
             if (user) {//esta autenticado
@@ -62,6 +69,19 @@ export class HomePage {
               localStorage.setItem('user', 'null');
               localStorage.setItem('number', 'null');
             }
+            this.dismissLoading()
           });
     }//fin de funcion verificar usuario
+    async presentLoading() {
+      this.loading = await this.loadingController.create({
+        message: 'Iniciando sesión...', // Puedes personalizar el mensaje del loader
+        translucent: true, // Permite que el fondo sea semi-transparente
+        spinner: 'crescent' // Elige el estilo del spinner (puedes utilizar 'lines', 'circles', 'crescent', 'dots' o 'lines-small')
+      });
+      await this.loading.present();
+    }
+    
+    async dismissLoading() {
+      await this.loading.dismiss();
+    }
 }
--- src/app/rutero-dia/rutero-dia.page.ts
+++ src/app/rutero-dia/rutero-dia.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CargaRuteroService } from "../servicios/firebase/carga-rutero.service";
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-rutero-dia',
@@ -10,18 +11,34 @@ import { ActivatedRoute } from '@angular/router';
 export class RuteroDiaPage implements OnInit {
   items: any;
   sinresult=false;
-  constructor(public ruteroService: CargaRuteroService, private route: ActivatedRoute) { }
+  constructor(public ruteroService: CargaRuteroService, private route: ActivatedRoute, private loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.presentLoading();
     const dia = this.route.snapshot.paramMap.get('dia');
     const number = localStorage.getItem('number');
     this.ruteroService.getRutero(number).subscribe((res) => {
       console.log(res);
       this.items = res.filter((item: any) => item.Dias === dia && !item.update)
-        .sort((a: any, b: any) => parseInt(a['Orden Dia']) - parseInt(b['Orden Dia']));
+        .sort((a: any, b: any) => parseInt(a['OrdenDia']) - parseInt(b['OrdenDia']));
       console.log(this.items);
       this.sinresult = this.items.length === 0;
+      this.dismissLoading();
     });
   }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Cargando...', // Puedes personalizar el mensaje del loader
+      translucent: true, // Permite que el fondo sea semi-transparente
+      spinner: 'crescent' // Elige el estilo del spinner (puedes utilizar 'lines', 'circles', 'crescent', 'dots' o 'lines-small')
+    });
+    await loading.present();
+  }
+  
+  async dismissLoading() {
+    await this.loadingController.dismiss();
+  }
+
+
 
 }
